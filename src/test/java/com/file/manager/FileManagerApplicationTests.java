628509bb6b1ac7b8013432510015b073d0269a3b
ElinaValieva <veaufa@mail.ru>
2019-02-23 13:20:19 +0300

    refactor: remove junit and selenium old tests

---
+++ src/test/java/com/file/manager/FileManagerApplicationTests.java
@@ -0,0 +1,20 @@
+package com.file.manager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * author: ElinaValieva on 23.02.2019
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileManagerApplicationTests {
+
+    @Test
+    public void contextLoad() {
+    }
+}
--- src/test/java/com/file/manager/selenium/config/SeleniumConfig.java
+++
@@ -1,8 +0,0 @@
-package com.file.manager.selenium.config;
-
-public class SeleniumConfig {
-
-    public static final String URL = "http://localhost:8083/";
-
-    public static final Integer TIMEOUT = 20;
-}
